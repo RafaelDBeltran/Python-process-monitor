dfa3acd6897dade8b742ebdb4d5a195c17aa1a80
RDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2020-11-06 16:24:33 -0300

    CPU usage

--- monitor.py
+++ monitor.py
@@ -7,8 +7,16 @@ def get_time(process):
     except OSError:
         print('Error on {}'.format(__name__))
 
+def get_cpu_usage(process):
+    try:
+        return len(process.cpu_affinity()), process.cpu_percent()
+    except psu.AccessDenied:
+        return 0, process.cpu_percent()
+
 for process in psu.process_iter():
     with process.oneshot():
         #take process name
         name = process.name()
         create_time = get_time(process)
+        cores, cpu_usage = get_cpu_usage(process)
+        print(cpu_usage)
