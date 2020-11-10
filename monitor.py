ecbd23cbe179822faffe193210feaa521896084b
RDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2020-11-10 17:04:23 -0300

    .

--- monitor.py
+++ monitor.py
@@ -1,78 +1,131 @@
 import psutil as psu
 import pandas as pd
 from datetime import datetime
+import time
+import os
 
-def get_time(process):
-    try:
-        return datetime.fromtimestamp(psu.boot_time())
-    except OSError:
-        print('Error on {}'.format(get_time.__name__))
+class Monitor:
 
-def get_cpu_usage(process):
-    try:
-        return len(process.cpu_affinity()), process.cpu_percent()
-    except psu.AccessDenied:
-        return 0, process.cpu_percent()
+    def __init__(self, PROCESS_SEARCH):
+        self.PROCESS_SEARCH = PROCESS_SEARCH
+        try:
+            while True:
 
-def get_process_piority(process):
-    try:
-        return int(process.nice())
-    except psu.AccessDenied:
-        return 0
+                self.data = []
 
-def get_memory_use(process):
-    try:
-        return process.memory_full_info().uss
-    except psu.AccessDenied:
-        return 0
+                for process in psu.process_iter():
+                    with process.oneshot():
+                        self.data.append({
+                        'PID': self.get_pid(process),
+                        'Name': self.get_process_name(process),
+                        'Create_time': self.get_time(process),
+                        'Cores': self.get_cpu_usage(process)[0],
+                        'Cpu_usage': self.get_cpu_usage(process)[1],
+                        'Status': self.get_process_status(process),
+                        'Priority': self.get_process_piority(process),
+                        'Memory_usage': self.get_memory_use(process),
+                        'Read_bytes': self.bytes_behavior(process)[0],
+                        'Write_Bytes': self.bytes_behavior(process)[1],
+                        'Threads_number': self.get_threads(process),
+                        'Command': self.get_command(process)
+                        })
 
-def bytes_behavior(process):
-    try:
-        io_counters = process.io_counters()
-        return io_counters.read_bytes, io_counters.write_bytes
-    except psu.AccessDenied:
-        print('Error on {}'.format(bytes_behavior.__name__))
-        return 0, 0
+                df = pd.DataFrame(self.data)
+                df.set_index('PID', inplace = True)
+                df['Memory_usage'] = df['Memory_usage'].apply(self.get_size)
+                df['Write_Bytes'] = df['Write_Bytes'].apply(self.get_size)
+                df['Read_bytes'] = df['Read_bytes'].apply(self.get_size)
 
-def get_threads(process):
-    try:
-        return process.num_threads()
-    except OSError:
-        print('Error on {}'.format(get_threads.__name__))
+                for i in df.index:
+                    for j in (df.loc[i,'Command']):
+                        if self.PROCESS_SEARCH in j:
+                            df.loc[i,'Command'] = self.PROCESS_SEARCH
+                        else:
+                            df.loc[i,'Command'] = 'nop'
+                        
+                df2 = df[df['Command'] == self.PROCESS_SEARCH]
 
-def get_process_name(process):
-    try:
-        return process.name()
-    except psu.AccessDenied:
-        return "Process not found"
+                os.system("clear")    
+                time.sleep(0.02)
 
-def get_process_status(process):
-    try:
-        return process.status()
-    except psu.AccessDenied:
-        return "Status not found"
+                print(df2)
+                df2.to_csv ('./out.csv', index = False, header=False, mode='a+')
+        except:
+            print('Exit program')
 
 
-data = []
+    def get_pid(self, process):
+        try:
+            return process.pid
+        except psu.AccessDenied:
+            return 0
 
-for process in psu.process_iter():
-    with process.oneshot():
-        data.append({
-        'Name': get_process_name(process),
-        'Create_time': get_time(process),
-        'Cores': get_cpu_usage(process)[0],
-        'Cpu_usage': get_cpu_usage(process)[1],
-        'Status': get_process_status(process),
-        'Priority': get_process_piority(process),
-        'Memory_usage': get_memory_use(process),
-        'Read_bytes': bytes_behavior(process)[0],
-        'Write_Bytes': bytes_behavior(process)[1],
-        'Threads_number': get_threads(process)
-        })
+    def get_time(self, process):
+        try:
+            now = datetime.now()
+            return now.strftime("%H:%M:%S")
+        except OSError:
+            print('Error on {}'.format(self.get_time.__name__))
 
-for i in data:
-    print(i)
+    def get_cpu_usage(self, process):
+        try:
+            return len(process.cpu_affinity()), process.cpu_percent()
+        except psu.AccessDenied:
+            return 0, process.cpu_percent()
+
+    def get_process_piority(self, process):
+        try:
+            return int(process.nice())
+        except psu.AccessDenied:
+            return 0
+
+    def get_memory_use(self, process):
+        try:
+            return process.memory_full_info().uss
+        except psu.AccessDenied:
+            return 0
+
+    def bytes_behavior(self, process):
+        try:
+            io_counters = process.io_counters()
+            return io_counters.read_bytes, io_counters.write_bytes
+        except psu.AccessDenied:
+            #print('Error on {}'.format(bytes_behavior.__name__))
+            return 0, 0
+
+    def get_threads(self, process):
+        try:
+            return process.num_threads()
+        except OSError:
+            print('Error on {}'.format(self.get_threads.__name__))
+
+    def get_process_name(self, process):
+        try:
+            return process.name()
+        except psu.AccessDenied:
+            return "Process not found"
+
+    def get_process_status(self, process):
+        try:
+            return process.status()
+        except psu.AccessDenied:
+            return "Status not found"
+
+    def get_size(self, bytes):
+        for unit in ['', 'K', 'M', 'G', 'T', 'P']:
+            if bytes < 1024:
+                return f"{bytes:.2f}{unit}B"
+            bytes /= 1024
+
+    def get_command(self, process):
+        try:
+            return process.cmdline()
+        except psu.AccessDenied:
+            return 'Not found'
+
 
 
 if __name__ == "__main__":
-    print('A')
+    x = Monitor('monitor.py')
+
+    
